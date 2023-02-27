308da898b469fed92353b8b674a32973f1abac87
stealthman22 <tdotonline@gmail.com>
2023-02-27 20:39:12 +0000

    adding all schemas, and getting sanity set

--- backend-sanity/alarajoel-portfolio/schemas/index.js
+++ backend-sanity/alarajoel-portfolio/schemas/index.js
@@ -1,4 +1,20 @@
 // calling in the testimonial schema
 import testimonials from './testimonials'
+import workExperience from './workExperience'
+import works from './works'
+import contact from './contact'
+import abouts from './abouts'
+import brands from './brands'
+import skills from './skills'
+import experiences from './experiences'
 
-export const schemaTypes = [testimonials]
+export const schemaTypes = [
+  works,
+  testimonials,
+  brands,
+  abouts,
+  skills,
+  workExperience,
+  experiences,
+  contact,
+]
