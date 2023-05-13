f3227b1016fc801a8a40f850e75307c03b985a27
stealthman22 <tdotonline@gmail.com>
2023-05-13 23:02:38 +0000

    gettng back to it

--- frontend-react/src/containers/index.js
+++ frontend-react/src/containers/index.js
@@ -1,3 +1,4 @@
+//  Prevents us from messing up the app.js files with imports
 export { default as About } from "./About/About";
 export { default as Footer } from "./Footer/Footer";
 export { default as Header } from "./Header/Header";
